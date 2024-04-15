d4565f94e859e9accd6ba34ac81c95830f347442
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-04-15 09:02:42 -0300

    AgregarProyectos()

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let proyectos = [];
-let tareas = [];
+var proyectos = [];
+var tareasTemp = [];
+
+function AgregarProyecto(){
+    const tiempo = new Date().getTime();
+    const proyectoInput = document.getElementById("nombreProyecto");
+    const descProyectoInput = document.getElementById("descProyecto");
+    const proyecto = proyectoInput.value.trim();
+    const desc = descProyectoInput.value.trim();
+
+    proyectos.push({
+        name: proyectoInput,
+        desc: descProyectoInput,
+        fechaCreacion: tiempo,
+        tareas: []
+    });
+    
+    mostrarProyectos();
+}
 
